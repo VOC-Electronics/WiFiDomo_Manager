181fb0b6bb3deb7c50ab0ec64e8bcb68651a7f69
Martijn van Leeuwen <info@voc-electronics.com>
2016-07-13 09:44:07 +0200

    Enabled debug and track SQLAlchemeny

--- wifidomoconfig.py
+++ wifidomoconfig.py
@@ -2,10 +2,11 @@ import os
 
 _basedir = os.path.abspath(os.path.dirname(__file__))
 
-DEBUG = False
+DEBUG = True
 
 DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'app/db/wifidomo.db')
 DB_FILE = os.path.join(_basedir, 'app/db/wifidomo.db')
 DATABASE_CONNECT_OPTIONS = {}
+SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 del os
