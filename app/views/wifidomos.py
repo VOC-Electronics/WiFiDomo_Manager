aba5af8d7657502ffcf1d5204b7435a6f94f09a9
Martijn van Leeuwen <info@voc-electronics.com>
2016-08-03 21:51:02 +0200

    Update wifidomos code

--- app/views/wifidomos.py
+++ app/views/wifidomos.py
@@ -19,9 +19,9 @@ __email__ = 'info@voc-electronics.com'
 from flask import Blueprint, render_template, session, redirect, url_for, request, flash, g, jsonify, abort
 from app.utils import requires_login, request_wants_json
 from app.wifidomo_manager import verify_password
-from app.wifidomo_manager import nav
+from app.wifidomo_manager import nav, app
 from collections import OrderedDict
-from app.database import WiFiDomo, db_session
+from app.database import WiFiDomo, Locations, db_session
 
 mod = Blueprint('wifidomos', __name__,
                 url_prefix='/wifidomo',
@@ -30,86 +30,129 @@ mod = Blueprint('wifidomos', __name__,
                 )
 
 # Sub-Website navigation:
-nav.Bar('subtop', [
+nav.Bar('subtopWD', [
   nav.Item('New', 'wifidomos.add_wifidomo'),
   nav.Item('Overview', 'wifidomos.overview')
 ])
 
 default_location_keys = {'location_id','location_name'}
 
+def get_location_list():
+  location_list = []
+  tempList = []
+  locations = Locations.query.all()
+  temp = {}
+  for location in locations:
+    temp = { str(location.id), str(location.location_name) }
+    tempList.append(dict(zip(default_location_keys, temp)))
+    print(location.location_name)
+  print(tempList.sort())
+  return tempList
+
+def get_location_list_old():
+  location_list = []
+  tempList = []
+  # ToDo: Cleanup the lowerpart of the code as we now have a working database creation script with test data.
+  wifidomo_location_0 = {'1', 'Unknown'}
+  wifidomo_location_1 = {'2', 'Living'}
+  tempList.append(dict(zip(default_location_keys, wifidomo_location_0)))
+  tempList.append(dict(zip(default_location_keys, wifidomo_location_1)))
+  tempList.sort()
+  print('templist')
+  print(tempList)
+  location_list = tempList
+  return location_list
 
 @mod.route('/')
 def index():
-#ToDo: Select available WifiDomo's from the database and parse them into the template.
-#  if g.user is None or (not g.user.is_admin):
-#    abort(401)
-  return render_template('wifidomos/index.html')
+  nr_wifidomo = WiFiDomo.query.count()
+  return render_template('wifidomos/index.html',
+                         nr_wifidomo=nr_wifidomo)
 
 
-@mod.route('/overview', methods=['POST'])
+@mod.route('/overview', methods=['GET'])
 #@requires_login
 def overview():
-  return render_template('wifidomos/overview.html')
+  # ToDo: Select available WifiDomo's from the database and parse them into the template.
+  nr_wifidomo = WiFiDomo.query.count()
+  flash(u'Not much available now, maybe in a future release')
+  return render_template('wifidomos/index.html',
+                         nr_wifidomo=nr_wifidomo)
 
-@mod.route('/add/', methods=['GET', 'POST'])
+@mod.route('/add/', methods=['GET', 'POST', 'PUT'])
 #@requires_login
 def add_wifidomo():
-  error = None
-  location_id = None
-  tempList = []
-
+  tempList = get_location_list_old()
+  if request.method == 'PUT':
+    if app.debug:
+      print('Processing PUT call.')
+    tempList = get_location_list()
+    return render_template('wifidomos/new.html',
+                           wifidomo_locations=tempList)
   if request.method == 'POST':
+    if app.debug:
+      print('Processing POST call.')
     location_id = request.form.get('location', type=int)
     name = request.form['name']
     fqdn = request.form['fqdn']
-    mac = request.form['mac']
+    mac = request.form.get('mac', type=str)
     ip4 = request.form['ip4']
-    status = request.form['status']
+    status = request.form.get('status', type=bool)
+    ip6 = request.form.get('ip6', type=str)
 
-    print("Submitted data:")
-    print(name)
-    print(fqdn)
-    print(mac)
-    print (ip4)
-    print(location_id)
-    if len(request.form['ip6']) > 1:
-      ip6 = request.form.get('ip6', type=str)
-    else:
+    if not mac:
+      mac = 'unknown'
+
+    if not ip4:
+      ip4 = '0.0.0.0'
+
+    if not status:
+      status = False
+
+    if not ip4:
+      ip4='0.0.0.0'
+
+    if not ip6:
       ip6 = ':0'
 
     if len(name) < 1:
       flash(u'Error: you have to enter a name')
-    elif len(fqdn)<12:
-      flash(u'Error: Full Qualified Domain Name required')
-    elif not ip4:
-      flash(u'Please proivde the valid ip4')
-    else:
-      location = 1
-      #location = Location.query.get(location_id)
-      if location is not None:
-        wifidomo = WiFiDomo(name, mac, location_id, fqdn, status, ip4, ip6)
-        db_session.add(wifidomo)
-        db_session.commit()
-        flash(u'Your wifidomo was added')
-        return redirect('/')
+      return render_template('wifidomos/new.html',
+                             wifidomo_locations=tempList)
+
+    location = Locations.query.get(location_id)
+
+    if app.debug:
+      print('Post')
+      print("Submitted data:")
+      print('Name: %s' % name)
+      print('Location id: %s' % str(location_id))
+      print('fqdn: %s' % fqdn)
+      print('Mac: %s' % mac)
+      print ('ip4: %s' % ip4)
+      print('Status: %s' % status)
+      print('ip6: %s' % str(ip6))
+      print('Location: %s' % str(location.location_name))
+      print('Check Location')
+
+
+    if location is not None:
+      wifidomo = WiFiDomo(name, mac, location_id, fqdn, status, ip4, ip6)
+      db_session.add(wifidomo)
+      db_session.commit()
+      flash(u'Your wifidomo was added')
+      return redirect('/')
     #ToDo: Cleanup the lowerpart of the code as we now have a working database creation script with test data
-    wifidomo_location_0 = {'Unknown', '0'}
-    wifidomo_location_1 = {'1', 'Livingroom'}
-    tempList.append(dict(zip(default_location_keys, wifidomo_location_0)))
-    tempList.append(dict(zip(default_location_keys, wifidomo_location_1)))
+    tempList = get_location_list_old()
     return render_template('wifidomos/new.html',
                            wifidomo_locations=tempList)
 
   if request.method == 'GET':
+    if app.debug:
+      print('Processing GET call.')
     # Query the database to get all locations
     # Now use only ony.
-    #ToDo: Cleanup the lowerpart of the code as we now have a working database creation script with test data.
-    wifidomo_location_0 = {'Unknown', '0'}
-    wifidomo_location_1 = {'1', 'Livingroom'}
-    tempList.append(dict(zip(default_location_keys, wifidomo_location_0)))
-    tempList.append(dict(zip(default_location_keys, wifidomo_location_1)))
-    print(tempList)
-
+    tempList = get_location_list()
     return render_template('wifidomos/new.html',
                            wifidomo_locations=tempList)
 
