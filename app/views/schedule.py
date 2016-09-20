560939b722932ec5507a342907ec80bc36959a1d
Martijn van Leeuwen <info@voc-electronics.com>
2016-09-20 22:41:26 +0200

    Updated .gitignore

---
+++ app/views/schedule.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Martijn van Leeuwen'
+__email__ = 'info@voc-electronics.com'
+
+'''
+# =[ DISCLAIMER ]===============================================================
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# ==============================================================================
+#
+# Todo:
+#
+# Get data from input parameters.
+# Check connections
+# Login
+# Update
+# ==============================================================================
+# Imports
+# ==============================================================================
+'''
+
+from datetime import datetime, timedelta
+from crontab import CronTab
+from collections import OrderedDict
+from flask import Blueprint, render_template, session, redirect, url_for, request, flash, g, \
+  jsonify, abort, abort, request, Markup
+from app.database import Preset, db_session, Schedules
+from app.wifidomo_manager import app, db, nav
+
+TIMEDELTA_UNITS = (
+    ('year',   3600 * 24 * 365),
+    ('month',  3600 * 24 * 30),
+    ('week',   3600 * 24 * 7),
+    ('day',    3600 * 24),
+    ('hour',   3600),
+    ('minute', 60),
+    ('second', 1)
+)
+
+empty_cron = CronTab()
+
+
+def format_datetime(dt):
+  return dt.strftime('%Y-%m-%d @ %H:%M')
+
+
+def format_date(dt):
+  return dt.strftime('%Y-%m-%d')
+
+
+def format_timedelta(delta, granularity='second', threshold=.85):
+  if isinstance(delta, datetime):
+    delta = datetime.utcnow() - delta
+  if isinstance(delta, timedelta):
+    seconds = int((delta.days * 86400) + delta.seconds)
+  else:
+    seconds = delta
+
+  for unit, secs_per_unit in TIMEDELTA_UNITS:
+    value = abs(seconds) / secs_per_unit
+    if value >= threshold or unit == granularity:
+      if unit == granularity and value > 0:
+        value = max(1, value)
+      value = int(round(value))
+      rv = u'%s %s' % (value, unit)
+      if value != 1:
+        rv += u's'
+      return rv
+  return u''
+
+mod = Blueprint('schedule', __name__,
+                static_folder='static')
+
+@mod.route('/')
+def index():
+  #nr_active_schedules = Schedules.query.where(active=true)
+  nr_schedules = Schedules.query.count()
+  schedules = Schedules.query.all()
+  return render_template('schedule/index.html',
+                         nr_active_schedules = nr_active_schedules,
+                         nr_schedules = nr_schedules,
+                         schedules = schedules)
+
+@mod.route('/edit/<int:id>,', methods=['GET', 'POST'])
+def edit_schedule(id):
+  error = None
+  data = Schedule.query.get(id)
+  if data is None:
+    abort(404)
