975123e1c3d3b933c8e580c84a383bbb7ed09581
Martijn van Leeuwen <info@voc-electronics.com>
2017-01-17 00:21:00 +0100

    Added disclaimer

--- run.py
+++ run.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python
-# Copyright (C) 2016, V.O.C. van Leeuwen
+# Copyright (C) 2016, V.O.C. Vanleeuwen Opensource Consultancy
 # -*- coding: utf-8 -*-
 __author__ = 'Martijn van Leeuwen'
 __email__ = 'info@voc-electronics.com'
+
+'''
+# =[ DISCLAIMER ]===============================================================
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# ==============================================================================
+'''
+
 from app.wifidomo_manager import app
 from modules.config import config
 
